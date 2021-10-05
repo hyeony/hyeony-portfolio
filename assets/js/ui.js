307bfbfd09bd91582099f723295587d304d17b5a
jihyeon.kim <jihyeon.kim@h9works.com>
2021-10-05 09:35:24 +0900

    스타일 추가

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   gnbScroll();
 
+  //gnb scroll
   function gnbScroll() {
     var gnb = $('.gnb');
 
@@ -12,48 +13,46 @@ $(document).ready(function () {
   }
 
   $(window).on('scroll', function () {
-    gnbScroll();
-
-    var con_scrollTop = $('.frame').offset().top;
-
-    console.log($(window).scrollTop());
-    console.log(con_scrollTop);
-    console.log(con_scrollTop - 180);
-
-    if ($(window).scrollTop() > con_scrollTop) {
-      $('.con_l').addClass('fix');
+    if ($('.gnb').length > 0) {
+      gnbScroll();
     }
   });
 
-  //new키워드를 이용해 새로운 인스턴스 생성
-  var controller = new ScrollMagic.Controller();
-  //Animation Object 생성
-  var tween1 = TweenMax.to('.circle', 0.5, {
-    autoAlpha: 1,
-    scale: 1.2,
-    y: 10
+  //follow img action
+  $('.work').bind('mousemove', function (e) {
+    $('.work_hover_img').css({
+      left: e.offsetX - 80,
+      top: e.offsetY - 120
+    });
   });
-  //Scene Object 생성, Animation Object를 Scene Object에 추가
-  //Scene Object를 ScrollMagic Controller에 추가
-  var scene = new ScrollMagic.Scene({
-      triggerElement: '.trigger1',
-    })
-    .setTween(tween1)
-    .addTo(controller)
-    .addIndicators({
-      name: '1'
-    });
 
-  var scene2 = new ScrollMagic.Scene({
-      triggerElement: '.trigger2', //트리거설정
-      // viewport에 대해 상대적으로 어느시점에 보여줄 것인지설정
-      // 0~1사이의 숫자를 입력하거나 문자열도가능. 디폴트는 onCenter(0.5)
-      triggerHook: 0.9
-    })
-    .setClassToggle('.square', 'visible')
-    .addTo(controller)
-    .addIndicators({
-      name: '2'
+  //scrollMagic Animation
+  var controller = new ScrollMagic.Controller();
+
+  //my
+  if ($('.my_wrap_con').length > 0) {
+    $('.my_wrap_con').each(function () {
+      var ourScene = new ScrollMagic.Scene({
+          triggerElement: this.children[0],
+          triggerHook: 0.9,
+        })
+        .setClassToggle(this, 'is-active')
+        .addTo(controller);
     });
+  }
+
+  //about
+  if ($('.about_wrap').length > 0) {
+    new ScrollMagic.Scene({
+        triggerHook: 0.7,
+        triggerElement: ".about_wrap",
+        duration: $(window).outerHeight()
+      })
+      .setTween(".about_wrap_inner", {
+        y: "-20%",
+        ease: Linear.easeNone
+      })
+      .addTo(controller);
+  }
 
 });
