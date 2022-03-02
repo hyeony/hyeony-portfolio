7c000dc2a915c85d4fd535ca29569ba3ea75b97c
jihyeon.kim <jihyeon.kim@h9works.com>
2022-03-02 17:51:10 +0900

    vue 스터디

---
+++ vuedongsan/src/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img :src="원룸.image" class="room-img">
+    <h4 @click="$emit('openModal')" >{{원룸.title}}</h4>
+    <!-- <h4  @click="모달창열렸니 = true, 누른거=i"> {{원룸들[i].title}} </h4> -->
+    <p> {{원룸.price}} 원</p>
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      원룸 : Object
+    }
+  }
+</script>
---
+++ vuedongsan/src/oneroom.js
@@ -0,0 +1,48 @@
+// var apple = 10;
+// var apple2 = 100;
+// // export default apple
+// export {apple, apple2}
+
+//어레이 안에 오브젝트
+export default [{
+    id : 0,
+    title: "Sinrim station 30 meters away",
+    image: "https://show.hnine.com/sandbox/assets/image/keyvisual/introduce_pc.png",
+    content: "18년 신축공사한 남향 원룸 ☀️, 공기청정기 제공",
+    price: 340000
+    },
+    {
+    id : 1,
+    title: "Changdong Aurora Bedroom(Queen-size)",
+    image: "https://show.hnine.com/sandbox/assets/image/keyvisual/apply_pc.png",
+    content: "침실만 따로 있는 공용 셰어하우스입니다. 최대 2인 가능",
+    price: 450000
+    },
+    {
+    id : 2,
+    title: "Geumsan Apartment Flat",
+    image: "https://show.hnine.com/sandbox/assets/image/keyvisual/result_pc.png",
+    content: "금산오거리 역세권 아파트입니다. 애완동물 불가능 🐶",
+    price: 780000
+    },
+    {
+    id : 3,
+    title: "Double styled beds Studio Apt",
+    image: "https://show.hnine.com/sandbox/assets/image/keyvisual/apply_pc.png",
+    content: "무암동인근 2인용 원룸입니다. 전세 전환가능",
+    price: 550000
+    },
+    {
+    id : 4,
+    title: "MyeongIl Apartment flat",
+    image: "https://show.hnine.com/sandbox/assets/image/keyvisual/result_pc.png",
+    content: "탄천동 아파트 월세, 남향, 역 5분거리, 허위매물아님",
+    price: 680000
+    },
+    {
+    id : 5,
+    title: "Banziha One Room",
+    image: "https://show.hnine.com/sandbox/assets/image/keyvisual/result_pc.png",
+    content: "반지하 원룸입니다. 비올 때 물가끔 새는거 빼면 좋아요",
+    price: 370000
+  }];
