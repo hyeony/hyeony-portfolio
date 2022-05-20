42aa3f17088c69cf18d5477bf4913d30e4da98da
jihyeon.kim <jihyeon.kim@h9works.com>
2022-05-20 15:19:01 +0900

    add file

---
+++ jihyeonFolio/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="box">
+      box1
+    </div>
+  </div>
+</template>
+
+<script>
+  import { onMounted, ref} from 'vue'
+  import axios from 'axios'
+  import { gsap } from "gsap"
+  import { ScrollTrigger, ScrollToPlugin, MotionPathPlugin } from "gsap/all"
+  gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, MotionPathPlugin)
+
+
+ 
+  export default {
+    name: 'portFolio',
+    components: {
+
+    },
+    data() {
+
+    },
+
+    methods: {
+      //Timline animation
+      scrollAnimation(){
+        gsap.timeline({
+          scrollTrigger: {
+            trigger: ".safe",
+            start: "-90%",
+            end: "-8% top",
+            markers: true,
+            scrub: true,
+          }
+        })
+        .from(".section-head-typo",{ transform: "matrix(1.5, 0, 0, 1.5, 0, 0)" })
+        .from(".section-head-sub", { transform: "matrix(1, 0, 0, 1, 0, 50)", opacity:"0"})
+
+        gsap.timeline({
+
+        })
+      },
+    }
+  }
+</script>
+
+<style lang="scss">
+  body {
+    margin: 0;
+  }
+
+  .box {
+    width: 50px;
+    height: 50px;
+    border: 1px solid red;
+  }
+</style>
--- jihyeonFolio/vue.config.js
+++ jihyeonFolio/vue.config.js
@@ -12,7 +12,8 @@ module.exports = defineConfig({
         `
       }
     }
-  }
+  },
+  publicPath:'/jihyeonPortFolio',
 })
 
 
