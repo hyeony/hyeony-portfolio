1f9e150c1fa8f7d961c254f3338a8d3a006c96ac
jihyeon.kim <jihyeon.kim@h9works.com>
2022-05-31 09:48:31 +0900

    불필요한 파일 삭제 및 컨텐츠 업데이트

--- jihyeonFolio/src/App.vue
+++ jihyeonFolio/src/App.vue
@@ -7,15 +7,15 @@
             Web UI Development,<br>
             Publisher.
           </div>
-          <p>안녕하세요 웹퍼블리셔 김지현입니다.<br> UI개발 외 UI,UX, 디자인에도 관심이 많습니다</p>
+          <p>안녕하세요 웹퍼블리셔 김지현입니다.<br> UI개발 외 UI,UX, 디자인에도 관심이 많습니다.<br> 현재 포트폴리오 리뉴얼작업중입니다 :)</p>
         </div>
       </div>
     </section>
     <div class="projects">
       <section class="middle">
-        <h1 class="middle-title">IPPP SSSS</h1>
+        <h1 class="middle-title">Project List</h1>
         <p class="middle-copy">
-          the mall's special offers. <br> the mall's special offers.
+          It's a list of all <br>the projects I've been working on
         </p>
       </section>
       <section class="bottom">
@@ -24,28 +24,40 @@
           <div class="list">
             <div class="row">
               <div class="item">
-                <a href="">
+                <a href="#!">
                   <div class="item-img one" :ref="itemImage">
-                  <img :src="require('@/assets/imgs/item_00.jpeg')" alt="">
+                  <img :src="require('@/assets/imgs/hds.png')" alt="">
                 </div>
                 </a>
-                <div>item 00</div>
+                <div class="name">SK하이닉스 디자인시스템 고도화프로젝트</div>
+                <p class="copy">프레임 워크 환경에서 스타일 작업 업무를 맡아 진행했습니다.</p>
               </div>
               <div class="item">
-                <a href="">
+                <a target="_blank" href="https://sandbox.fintech.or.kr/">
                   <div class="item-img two" :ref="itemImage">
-                    <img :src="require('@/assets/imgs/item_01.jpeg')" alt="">
+                    <img :src="require('@/assets/imgs/sandbox.png')" alt="">
                   </div>
                 </a>
-                <div>item 01</div>
+                <div class="name">금융규제 샌드박스</div>
+                <p class="copy">모든 ui개발을 담당하여 진행했습니다.</p>
               </div>
               <div class="item">
-                <a href="">
+                <a target="_blank" href="https://uxstudio.sktelecom.com/">
                   <div class="item-img three" :ref="itemImage">
-                  <img :src="require('@/assets/imgs/item_00.jpeg')" alt="">
+                  <img :src="require('@/assets/imgs/studio.png')" alt="">
                 </div>
                 </a>
-                <div>item 03</div>
+                <div class="name">UX Studio</div>
+                <p class="copy">부트스트랩을 이용하여 컴포넌트를 제작하는 업무를 진행했습니다.</p>
+              </div>
+              <div class="item">
+                <a target="_blank" href="https://uxstudio.sktelecom.com/">
+                  <div class="item-img four" :ref="itemImage">
+                  <img :src="require('@/assets/imgs/studio.png')" alt="">
+                </div>
+                </a>
+                <div class="name">~ studying</div>
+                <p class="copy">notion에 스터디내용을 정리하며 성장을 위해 노력하고있습니다</p>
               </div>
             </div>
           </div>
@@ -53,10 +65,14 @@
         </div>
       </section>
     </div>
-    <div class="bin"></div>
-    <div class="bin"></div>
-    <div class="bin"></div>
-    <div class="bin"></div>
+    <section class="resume">
+      <div class="container">
+        <a href="" class="btn-resume-down">Download resume </a>
+      </div>
+    </section>
+    <footer>
+      <div></div>
+    </footer>
   </div>
 </template>
 
@@ -97,10 +113,10 @@
             trigger: ".intro",
             start: "top top",
             scrub: true,
-            markers: true,
+            // markers: true,
           }
         })
-        .to(".intro-wrap",{ y:"100%", ease: "none"})
+        .to(".intro-wrap",{ y:"100%", ease: "none", backgroundColor: '#fff'})
 
 
         gsap.timeline({
@@ -122,6 +138,7 @@
           scrollTrigger: {
             trigger: ".item-img.one",
             start: "top bottom",
+            end:"bottom center",
             scrub: true,
             // markers: true,
           }
@@ -132,6 +149,7 @@
           scrollTrigger: {
             trigger: ".item-img.two",
             start: "top bottom",
+            end:"bottom center",
             scrub: true,
             // markers: true,
           }
@@ -142,11 +160,23 @@
           scrollTrigger: {
             trigger: ".item-img.three",
             start: "top bottom",
+            end:"bottom center",
             scrub: true,
             // markers: true,
           }
         })
         .to(".item-img.three",{ y:"0", ease: "none"})
+
+        gsap.timeline({
+          scrollTrigger: {
+            trigger: ".item-img.four",
+            start: "top bottom",
+            end:"bottom center",
+            scrub: true,
+            // markers: true,
+          }
+        })
+        .to(".item-img.four",{ y:"0", ease: "none"})
 
 
       },
@@ -162,9 +192,35 @@
       this.scrollAnimation();
       this. heightChange();
 
+      const sections = document.querySelectorAll('section');
+       const cards = document.querySelectorAll('.item-img');
+       console.log(cards);
+
+      sections.forEach((section, index) => {
+        gsap.to(section, {autoAlpha: 1,
+          scrollTrigger: {
+            trigger: section,
+            start: 'top bottom-=200',
+            toggleActions: 'play none none reverse',
+            // markers: true
+          }
+        });
+
+        ScrollTrigger.create({
+          trigger: section,
+          id: index+1,
+          start: 'top center',
+          end: () => `+=${section.clientHeight + 50}`,
+          toggleActions: 'play reverse none reverse',
+          toggleClass: {targets: section, className: "is-active"},
+          duration: 2
+          // markers: true
+        })
+
+      })
 
       //set refs array 
-      this.itemRefs.forEach(this.addTimeline);
+      // this.itemRefs.forEach(this.addTimeline);
     }
   }
 </script>
